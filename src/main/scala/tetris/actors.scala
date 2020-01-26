13944796baf113286f29b150e93968e7d773e7b4
Alex Boisvert <alexb@ginger.io>
2020-01-26 09:07:02 -0800

    Move updateState() to GameActorState

--- src/main/scala/tetris/actors.scala
+++ src/main/scala/tetris/actors.scala
@@ -12,7 +12,16 @@ case object Tick extends GameMessage[Unit]
 case object Drop extends GameMessage[Unit]
 case object GetView extends GameMessage[GameView]
 
-case class GameActorState(gameState: GameState, stateUpdatedHook: Task[Unit])
+case class GameActorState(gameState: GameState, stateUpdatedHook: Task[Unit]) {
+
+  /** Apply `transform` to GameSate and trigger `stateUpdatedHook` */
+  def update(transform: GameState => GameState): Task[(GameActorState, Unit)] = {
+    for {
+      newState <- IO.effectTotal { copy(gameState = transform(gameState)) }
+      _ <- stateUpdatedHook
+    } yield (newState, ())
+  }
+}
 
 object GameActor extends Actor.Stateful[GameActorState, Throwable, GameMessage] {
   import GameState._
@@ -23,24 +32,13 @@ object GameActor extends Actor.Stateful[GameActorState, Throwable, GameMessage] 
     context: Context
   ): Task[(GameActorState, T)] = {
     msg match {
-      case MoveLeft  => updateState(state) { moveLeft }
-      case MoveRight => updateState(state) { moveRight }
-      case Rotate90  => updateState(state) { rotate90 }
-      case Tick      => updateState(state) { tick }
-      case Drop      => updateState(state) { drop }
+      case MoveLeft  => state.update(moveLeft)
+      case MoveRight => state.update(moveRight)
+      case Rotate90  => state.update(rotate90)
+      case Tick      => state.update(tick)
+      case Drop      => state.update(drop)
       case GetView   => IO.effectTotal { (state, state.gameState.view) }
     }
-  }
-
-  private[this] def updateState(
-    state: GameActorState
-  )(
-    transform: GameState => GameState
-  ): Task[(GameActorState, Unit)] = {
-    for {
-      newState <- IO.effectTotal { state.copy(gameState = transform(state.gameState)) }
-      _ <- state.stateUpdatedHook
-    } yield (newState, ())
   }
 }
 
